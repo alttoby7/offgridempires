3e12fb9ae4874f06b299debc47933609a524e8f2
alttoby7 <156732088+alttoby7@users.noreply.github.com>
2026-03-23 14:24:35 -0600

    Remove Impact site verification meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -69,9 +69,7 @@ export default function RootLayout({
       className={`${inter.variable} ${jetbrainsMono.variable} h-full antialiased`}
     >
       <head>
-        {/* Impact affiliate site verification */}
-        <meta name="impact-site-verification" {...{ value: "26ccb1d9-e2c5-436e-84de-eac1b435dd35" } as object} />
-        <Script
+<Script
           src="https://www.googletagmanager.com/gtag/js?id=G-PGP7GKZ3ZT"
           strategy="afterInteractive"
         />
